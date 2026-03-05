6b37e0ac271c9eb9725b31c832d4ac023a60d72e
Charlie Whittleman <brennwh@siue.edu>
2026-03-05 08:38:23 -0600

    Improve serial.c readability

--- src/serial.c
+++ src/serial.c
@@ -1,16 +1,10 @@
 #include "serial.h"
+#include "io.h"
 
-static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
+#define COM1 0x3F8
 
 static int serial_tx_empty(void) {
-    return inb(0x3F8 + 5) & 0x40;
+    return inb(COM1 + 5) & 0x40;
 }
 
 void serial_flush(void) {
@@ -19,22 +13,22 @@ void serial_flush(void) {
 
 
 void serial_init(void) {
-    outb(0x3F8 + 1, 0x00);
-    outb(0x3F8 + 3, 0x80);
-    outb(0x3F8 + 0, 0x03);
-    outb(0x3F8 + 1, 0x00);
-    outb(0x3F8 + 3, 0x03);
-    outb(0x3F8 + 2, 0xC7);
-    outb(0x3F8 + 4, 0x0B);
+    outb(COM1 + 1, 0x00);   // Disable all interrupts
+    outb(COM1 + 3, 0x80);   // Enable DLAB (set baud rate divisor)
+    outb(COM1 + 0, 0x03);   // Set divisor to 3 (lo byte) — 38400 baud
+    outb(COM1 + 1, 0x00);   //                  (hi byte)
+    outb(COM1 + 3, 0x03);   // 8 bits, no parity, one stop bit
+    outb(COM1 + 2, 0xC7);   // Enable FIFO, clear them, 14-byte threshold
+    outb(COM1 + 4, 0x0B);   // IRQs enabled, RTS/DSR set
 }
 
 static int serial_can_tx(void) {
-    return inb(0x3F8 + 5) & 0x20;
+    return inb(COM1 + 5) & 0x20;
 }
 
 void serial_putc(char c) {
     while (!serial_can_tx()) {}
-    outb(0x3F8, (uint8_t)c);
+    outb(COM1, (uint8_t)c);
 }
 
 void serial_print(const char* s) {
