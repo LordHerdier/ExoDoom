f57d8254e089cf0027b4743022c2543fd674a7dc
charlotte whittleman <charlotte@whittleman.net>
2026-03-21 16:36:58 -0500

    Add string.h; Stub functions

---
+++ src/string.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+#include <stdint.h>
+
+size_t strlen(const char *s);
+
+int strcmp(const char *s1, const char *s2);
+
+void *memset(void *s, int c, size_t n);
+
+void *memcpy(void *dest, const void *src, size_t n);
+
+void *memmove(void *dest, const void *src, size_t n);
+
+void *memcmp(const void *s1, const void *s2, size_t n);
+
+char *strncpy(char *dest, const char *src, size_t n);
+
+char *strcat(char *dest, const char *src);
+
+char *strchr(const char *s, int c);
