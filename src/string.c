2fa0128fec2e53808c90f2db18c934da05ae8f17
Charlotte <brennwh@siue.edu>
2026-03-23 01:41:14 +0000

    [SCRUM-11] Implement string.h (memcpy, memset, memmove, strcmp, strlen, strncpy, strcat, strchr, memcmp) (#9)
    * Add string.h; Stub functions
    
    * Fixed import; implemented strlen()
    
    * Added basic tests for string.h
    
    * Implement strcmp() and tests
    
    * Implement memset(); tests
    
    * Implement memcpy(); tests
    
    * Implement memmove(); tests
    
    * Implement memcmp(); tests
    
    * Implement strncpy(); tests
    
    * Implement strcat(); tests
    
    * Implement strchr(); tests; last func implementation
    
    * Added usability comments and todo; ran formatter
    
    * Noted we need a proper unit testing pipeline

---
+++ src/string.c
@@ -0,0 +1,88 @@
+#include "string.h"
+
+size_t strlen(const char *s) {
+  const char *p = s;
+  while (*p)
+    p++;
+  return (size_t)(p - s);
+}
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return (unsigned char)*s1 - (unsigned char)*s2;
+}
+
+void *memset(void *s, int c, size_t n) {
+  unsigned char *p = s;
+  while (n--)
+    *p++ = (unsigned char)c;
+  return s;
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+  unsigned char *d = dest;
+  const unsigned char *s = src;
+  while (n--)
+    *d++ = *s++;
+  return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n) {
+  unsigned char *d = dest;
+  const unsigned char *s = src;
+  if (d < s || d >= s + n) {
+    while (n--)
+      *d++ = *s++;
+  } else {
+    d += n;
+    s += n;
+    while (n--)
+      *--d = *--s;
+  }
+  return dest;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+  const unsigned char *a = s1;
+  const unsigned char *b = s2;
+  while (n--) {
+    if (*a != *b)
+      return *a - *b;
+    a++;
+    b++;
+  }
+  return 0;
+}
+
+char *strchr(const char *s, int c) {
+  unsigned char ch = (unsigned char)c;
+  while (*s) {
+    if ((unsigned char)*s == ch)
+      return (char *)s;
+    s++;
+  }
+  return ch == '\0' ? (char *)s : NULL;
+}
+
+/* NOTE: strcat has no bounds checking! Caller is responsible for ensuring dest
+ * has sufficient space for dest + src + NUL terminator. */
+char *strcat(char *dest, const char *src) {
+  char *d = dest;
+  while (*d)
+    d++;
+  while ((*d++ = *src++))
+    ;
+  return dest;
+}
+
+char *strncpy(char *dest, const char *src, size_t n) {
+  size_t i;
+  for (i = 0; i < n && src[i]; i++)
+    dest[i] = src[i];
+  for (; i < n; i++)
+    dest[i] = '\0';
+  return dest;
+}
