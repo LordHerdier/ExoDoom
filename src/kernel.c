356617a2b966836f8456338defdf7cfca61a2b3b
charlotte whittleman <charlotte@whittleman.net>
2026-04-01 08:56:05 -0500

    Revert drift test print count to 5; restore framebuffer console demo

--- src/kernel.c
+++ src/kernel.c
@@ -9,6 +9,8 @@
 #include "pic.h"
 #include "pit.h"
 #include "sleep.h"
+#include "fb.h"
+#include "fb_console.h"
 
 //IRQ0 stub from assembly
 extern void irq0_stub();
@@ -50,7 +52,7 @@ void kernel_main(uint32_t mb_info_addr) {
 
     //Print monotonic ms counter for 5 seconds then exit
     uint32_t prints = 0;
-    while (prints < 105) {
+    while (prints < 5) {
         if (pit_take_print_pending()) {
             serial_print("ms: ");
             serial_print_u32(kernel_get_ticks_ms());
@@ -62,5 +64,24 @@ void kernel_main(uint32_t mb_info_addr) {
 
     qemu_exit(0);
 
-    (void)mb;
+    if (!(mb->flags & MULTIBOOT_INFO_FLAG_FRAMEBUFFER)) for(;;);
+
+    framebuffer_t fb;
+    if (!fb_init_bgrx8888(&fb,
+                          (uintptr_t)mb->framebuffer_addr,
+                          mb->framebuffer_pitch,
+                          mb->framebuffer_width,
+                          mb->framebuffer_height,
+                          mb->framebuffer_bpp)) {
+        for(;;);
+    }
+
+    fb_console_t con;
+    if (!fbcon_init(&con, &fb)) for(;;);
+
+    fbcon_set_color(&con, 255,255,255, 0,0,0);
+    fbcon_write(&con, "ExoDoom fb console online.\n");
+    fbcon_write(&con, "Now printing to pixels like a proper gremlin.\n\n");
+    fbcon_write(&con, "0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+    fbcon_write(&con, "abcdefghijklmnopqrstuvwxyz !@#$%^&*()[]{}\n");
 }
